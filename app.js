fe3befd42776413e0407e16b55988f41b0043f0d
Michele Leroux Bustamante <michelebusta@gmail.com>
2016-04-22 13:08:59 -0400

    change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var http = require('http');
 var app = express();
 
 app.get('/', function (request, response, next) {
-	response.send('I want coffee?');
+	response.send('the demo finally worked');
 });
 
 var httpServerPort = process.env.PORT || 8080;
