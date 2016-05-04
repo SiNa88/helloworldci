d28bfe8a23a488aad002ba8fb9c36b3864e3c768
Michele Leroux Bustamante <michelebusta@gmail.com>
2016-05-04 10:24:03 +0200

    updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var http = require('http');
 var app = express();
 
 app.get('/', function (request, response, next) {
-	response.send('the demo finally worked');
+	response.send('containers are fun');
 });
 
 var httpServerPort = process.env.PORT || 8080;
