ff882ef05cd2a5d7810037dd82ec5bf14957801c
Michele Leroux Bustamante <michelebusta@gmail.com>
2016-08-03 12:05:49 +1000

    new location

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var http = require('http');
 var app = express();
 
 app.get('/', function (request, response, next) {
-	response.send('hello from san diego!');
+	response.send('hello from sydney!');
 });
 
 var httpServerPort = process.env.PORT || 8080;
